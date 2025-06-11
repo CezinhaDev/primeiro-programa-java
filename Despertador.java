7617c21b406d2231d77eee9c6ba70a414ef19558
Cesar Henrique <suporte@nicempregos.com.br>
2025-06-11 15:11:37 -0300

    feat: como usar classes

---
+++ Despertador.java
@@ -0,0 +1,13 @@
+public class Despertador {
+    int horas;
+    int minutos;
+    int soneca;
+
+    void ativarSoneca(){
+        System.out.println("so mais " + soneca + " minutos :P");
+    }
+
+    String agendarAlarme() {
+        return "Seu alarme foi agendado para " + hora + ":" + minuto;
+    }
+}
